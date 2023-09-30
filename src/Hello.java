6b6b919a6bfa46642e2e3bb07c192b0cf84546cf
hoholro <tnrud321@gmail.com>
2023-09-30 23:01:22 +0900

    인텔리제이를 이용한 자바프로그래밍 분석

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String[] args){
-        System.out.println("hello");
+        int i = 1;
+        System.out.println(i);
+        System.out.println(200);
     }
 }
---
+++ src/MyProfile.java
@@ -0,0 +1,14 @@
+public class MyProfile {
+    public static void main(String[] args){
+        int i = 1;
+        while(i <10){
+            System.out.println("**********");
+            i = i +1;
+        }
+//      System.out.println("수경수");
+//      System.out.println("sususu@gmail.com");
+//      System.out.println("여자");
+    }
+}
+// main()메소드 안의 내용을 차례대로 출력한다
+//System.out.print 는 줄바꿈을 하지 않는다
