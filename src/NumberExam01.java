4e4ded8691a930ae6b514dd9e03488bb76821f48
sugyeongsu <tnrud321@gmail.com>
2023-10-04 00:11:15 +0900

    오버플로우 개념까지 완료

---
+++ src/NumberExam01.java
@@ -0,0 +1,22 @@
+import java.util.function.DoublePredicate;
+
+public class NumberExam01
+{
+    public static void main (String[] args){
+        int maxInt = Integer.MAX_VALUE;
+        int minInt = Integer.MIN_VALUE;
+
+        double maxDouble = Double.MAX_VALUE;
+        double minDouble = Double.MIN_VALUE;
+
+        System.out.println(maxInt);
+        System.out.println(minInt);
+
+        System.out.println(maxDouble);
+        System.out.println(minDouble);
+
+}
+}
+
+
+//정수의 최댓값과 최솟값 구하기
