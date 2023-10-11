78fdb519657e36f8ddba5f35dcade38393926680
hoholro <tnrud321@gmail.com>
2023-10-11 20:43:37 +0900

    조건문과 삼항연산자 완료

---
+++ src/IfExam3.java
@@ -0,0 +1,18 @@
+public class IfExam3 {
+    public static void main(String[] args) {
+        int score = 100;
+        if (score >= 90) {
+            System.out.println("A");
+        } else if (score >= 80) {
+            System.out.println("B");
+        } else if (score >= 70) {
+            System.out.println("C");
+        } else if (score >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+    }
+}
+
+//code -> reformat code 누르면 코드가 정리된다
---
+++ src/IfExam5.java
@@ -0,0 +1,11 @@
+public class IfExam5 {
+    public static void main(String[] args) {
+    //삼항연산자
+    //자바에는 항이 3개인 연산자가 하나 있다. 조건식이 참일 경우 반환값이 1이 사용되고, 거짓일 경우 반환값 2가 사용된다
+    //조건식 ? 반환값1 : 반환값2
+        int a = 10;
+        int value = (a > 5) ? 20 : 30;
+        System.out.println(value);
+    }
+}
+
