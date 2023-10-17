c728bc4da3a94dd75b780e0c283be33400a3fec6
hoholro <tnrud321@gmail.com>
2023-10-17 20:25:26 +0900

    반복문for

---
+++ src/VendingMachine.java
@@ -0,0 +1,6 @@
+public class VendingMachine {
+//    field란 : 자판기가 가지는것
+//    생성자 :
+//    method
+}
+
---
+++ src/VendingMachineMain.java
@@ -0,0 +1,19 @@
+public class VendingMachineMain {
+    public static void main (String[] args){
+        VendingMachineMain vm1 = new VendingMachineMain();
+//  static 이 붙은 메소드는 클래스 메소드라고 부른다
+//  클래스 메소드는 인스턴스를 생성하지 않아도 사용할수가 있다
+//  -> 메모리에 올라가 있다
+//  인스턴스로 메모리에 올라가야 오브젝트라고 부를수가 있다
+//  메소드에서 사용하는건 의존한다라고 생각하기
+//  VendingMachineMain은 String 클래스에도 의존한다
+//  클래스명 변수명 = new 클래스명();
+
+//  인스턴스를 만드는 세가지 방법
+//  1 .new 연산자와 생성자를 이용하여 인스턴스를 만드는 방법
+//  2. 클래스 로더를 이용하는 방법
+//  3. 메모리에 있는 인스턴스를 복제하여 만드는 방법
+
+        
+    }
+}
